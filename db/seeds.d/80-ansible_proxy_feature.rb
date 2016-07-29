601fa01d8134ce85357697cc2b66e2f35f1437fb
Ondrej Prazak <oprazak@redhat.com>
2016-07-29 08:05:10 +0200

    Change in job_template meta + Rails 4.2 compatibility

--- db/seeds.d/80-ansible_proxy_feature.rb
+++ db/seeds.d/80-ansible_proxy_feature.rb
@@ -1,2 +1,2 @@
-f = Feature.find_or_create_by_name('Ansible')
+f = Feature.find_or_create_by(:name => 'Ansible')
 raise "Unable to create proxy feature: #{format_errors f}" if f.nil? || f.errors.any?
